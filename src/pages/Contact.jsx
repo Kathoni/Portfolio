05d0efbcd09d006ef21b61ec3e446d226c08d628
Kathoni <thrusherkeys@gmail.com>
2024-08-11 18:20:42 +0300

    adjustments

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const ContactForm = () => {
   const [formData, setFormData] = useState({
@@ -22,9 +23,11 @@ const ContactForm = () => {
   };
 
   return (
-    <div className="container mt-5">
+    <div className="container mt-5"
+>
       <h2>Contact Me</h2>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit}
+      style={{width:"80%"}}>
         <div className="mb-3">
           <label htmlFor="name" className="form-label">Name</label>
           <input
@@ -75,6 +78,38 @@ const ContactForm = () => {
         </div>
         <button type="submit" className="btn btn-primary">Submit</button>
       </form>
+      <br />
+      <hr />
+      <div>
+        <h5> You can also reach me on social media.</h5>
+            <ul
+            style={{
+                display: "flex",
+                justifyContent: "space-evenly",
+                listStyle: "none",
+                margin: "0",
+                padding: "0",
+            }}>
+                <li style={{
+                    fontSize: "25px",
+                }}><Link to={"https://www.instagram.com/k_.tho.ni"}><i class="bi bi-instagram"  ></i></Link></li>
+                <li
+                style={{ fontSize: "25px" }}
+                ><Link to={"https://www.linkedin.com/in/sylvia-kathoni-869ab42a7/"}><i class="bi bi-linkedin"></i></Link></li>
+                <li
+                style={{ fontSize: "25px" }}><Link to={"https://github.com/Kathoni"}><i class="bi bi-github"></i></Link></li>
+            </ul>
+        <br />
+        <h3
+        style={{
+            textAlign: "center",
+            fontWeight: "light",
+            fontSize: "20px",
+            marginTop: "20px",
+            marginBottom: "20px",
+            fontFamily: "cursive",
+        }}>Made with love by Kathoni❤</h3>
+      </div>
     </div>
   );
 };
