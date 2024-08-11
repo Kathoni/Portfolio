83d273d1bef08d36d3fbdfaf932ee460dc4e21f5
Kathoni <thrusherkeys@gmail.com>
2024-08-11 10:55:27 +0300

    pages set up

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import Home from './pages/Home'
 
 function App() {
   return (
-    <div>App</div>
+    <div>
+      <Home/>
+    </div>
   )
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+        <h1>Hello. <br />Welcome to my page</h1>
+    </div>
+  )
+}
+
+export default Home
